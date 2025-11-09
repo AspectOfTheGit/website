c58c48ed7d2060c9363bc5ec5b19ae91a757b0b8
AspectOfTheGit <151046557+AspectOfTheGit@users.noreply.github.com>
2025-11-09 11:30:53 +0000

    Update app.py
    GOOB

--- src/app.py
+++ src/app.py
@@ -205,7 +205,7 @@ def raw_to_html(component):
             style.append("text-decoration: line-through")
             
         style_str = ";".join(styles + ["white-space:pre"])
-        html = f"<span style='{'; '.join(style)}'>{text}</span>" if style else text
+        html = f"<span style='{'; '.join(styles)}'>{text}</span>" if style else text
         
         for e in c.get("extra", []):
             html += to_html(e, {
