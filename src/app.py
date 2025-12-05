d0b7e833f5cd35f7b2ae898acb083508f0176704
AspectOfTheGit <151046557+AspectOfTheGit@users.noreply.github.com>
2025-12-05 14:28:02 +0000

    Update app.py
    moved

--- src/app.py
+++ src/app.py
@@ -410,8 +410,6 @@ def mc_login():
 
 @app.route("/bots/log", methods=["POST"])
 def update_log():
-    import time
-    
     token = request.headers.get("Authorization")
     if token != BOT_TOKEN:
         return jsonify({"error": "Unauthorized"}), 401
