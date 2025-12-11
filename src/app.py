b82520788189955bb976d55c7770adcf30b5e1e5
AspectOfTheGit <151046557+AspectOfTheGit@users.noreply.github.com>
2025-12-11 20:41:16 +0000

    Update app.py

--- src/app.py
+++ src/app.py
@@ -265,6 +265,9 @@ def accountpage():
         data["account"].setdefault(mcusername, {})
         data["account"][mcusername].setdefault("abilities", {}) # Stores player's permissions for what they can do on the website
         data["account"][mcusername].setdefault("storage", {}) # Stores the storage for the player's account (storage is a dictionary)
+        try:
+            if data["account"][account]["last_deploy"] != now.date():
+                data["account"][account]["used"] = 0
         return render_template("account.html", username=mcusername, account=data["account"][mcusername])
     else:
         return redirect("/login")
