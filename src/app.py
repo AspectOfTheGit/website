930a83640c97226c5c2f74c6d202c71206ddc443
AspectOfTheGit <151046557+AspectOfTheGit@users.noreply.github.com>
2026-01-04 22:05:11 +0000

    Update app.py

--- src/app.py
+++ src/app.py
@@ -415,7 +415,7 @@ def world_edit(world):
     global data
 
     data.setdefault("world", {})
-    if world not in data["world"]:
+    if world in data["world"]:
         mcusername = session.get("mc_username")
         if mcusername != data["world"][world]["owner"]:# Check if user owns the world
             return jsonify({"error": "Unauthorized"}), 401
