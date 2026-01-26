73d16cda5b7b61ab2e383edf8a18ea835590aed0
AspectOfTheGit <151046557+AspectOfTheGit@users.noreply.github.com>
2026-01-26 17:28:19 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,18 +3,21 @@ eventlet.monkey_patch()
 
 from flask import Flask
 from flask_socketio import SocketIO
-from app.data import load_data
+from src.data import load_data
+from src.config import CLIENT_SECRET
 
 socketio = SocketIO(cors_allowed_origins="*", async_mode="eventlet")
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = "..."
+    app.secret_key = CLIENT_SECRET
 
     load_data()
 
-    from app.bots.routes import bots_bp
+    from src.bots.routes import bots_bp
+    from src.web.routes import web
     app.register_blueprint(bots_bp)
+    app.register_blueprint(web)
 
     socketio.init_app(app)
     return app
