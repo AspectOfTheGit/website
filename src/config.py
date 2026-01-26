ff6326f192edd23f53c0289fca13b5a543b02131
AspectOfTheGit <151046557+AspectOfTheGit@users.noreply.github.com>
2026-01-26 10:28:20 +0000

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,13 @@
+import os
+
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+OTHER_TOKEN = os.environ.get("OTHER_TOKEN")
+DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
+
+GUILD_ID = 1460692900533899438
+REDIRECT_URI = "https://aspectofthe.site/login"
+DATA_FILE = "/data/values.json"
+
+TIMEOUT = 10
