5aef70503e11c08941b0bc32ad251f69aebb15a1
AspectOfTheGit <151046557+AspectOfTheGit@users.noreply.github.com>
2026-01-27 09:45:11 +0000

    Update player_api.py

--- src/utils/player_api.py
+++ src/utils/player_api.py
@@ -1,5 +1,7 @@
 import requests
 
+from src.data import data, save_data
+
 def get_username(uuid: str) -> str | None:
     uuid = uuid.replace("-", "")
     url = f"https://api.ashcon.app/mojang/v2/user/{uuid}"
@@ -34,3 +36,18 @@ def format_uuid(u):
     )
     return u
 
+def storagesize(account: str):
+    data["account"][account].setdefault("storage", {})
+    data["account"][account]["storage"].setdefault("capacity", {})
+    total = 0
+    try:
+        for i in data["account"][account]["storage"]["capacity"].keys():
+            total += data["account"][account]["storage"]["capacity"][i]
+    except:
+        try:
+            total = data["account"][account]["abilities"]["capacity"] * 1024 * 1024
+        except:
+            total = 10000000
+    data["account"][account]["storage"]["size"] = total
+
+    save_data()
