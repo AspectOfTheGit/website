cec9b77741584d16deb01700fc83e19beb240f32
AspectOfTheGit <151046557+AspectOfTheGit@users.noreply.github.com>
2026-01-28 11:59:25 +0000

    Update socket.py

--- src/socket.py
+++ src/socket.py
@@ -9,28 +9,30 @@ socketio = SocketIO(cors_allowed_origins="*", async_mode="eventlet")
 
 def emit_storage_log(account, message, event, world_id=None):
     ts = time.strftime('%H:%M:%S')
-    prefix = f"`[World {world_id}]` " if world_id else ""
+    prefix = f"`[World {world_id}]`" if world_id else ""
     contents = [ts, f"{prefix} {message}"]
 
     socketio.emit("log", contents, room=account)
     notify(account, contents[1], event)
+    print(f"[socket.py] Emitted storage log to '{account}': {contents[1]}")
 
 def emit_log(type, contents, room, notify=False, event=None):
     socketio.emit(type, contents, room=room)
     if notify:
         notify(room, contents[1], event)
+    print(f"[socket.py] Emitted log to '{room}'")
 
 
 # Events
 
 @socketio.on('connect')
 def handle_connect():
-    print('[app.py] Client connected')
+    print('[socket.py] Client connected')
 
 @socketio.on('join')
 def handle_join(room):
     join_room(room)
-    print(f'[app.py] Client joined room: {room}')
+    print(f'[socket.py] Client joined room: {room}')
 
 @socketio.on("get_screenshot")
 def screenshot_request(bot):
@@ -38,7 +40,7 @@ def screenshot_request(bot):
     if bot_name not in data["bot"]:
         return abort(400)
 
-    print(f"[app.py] Screenshot requested for {bot_name}")
+    print(f"[socket.py] Screenshot requested for {bot_name}")
 
     data["bot"][bot_name].setdefault("do", {})
     data["bot"][bot_name]["do"]["screenshot"] = True
