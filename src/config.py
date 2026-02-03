001b2dbee192745dadc4b6a46e47da7e5299ddc4
AspectOfTheGit <151046557+AspectOfTheGit@users.noreply.github.com>
2026-02-03 10:15:27 +0000

    Update config.py

--- src/config.py
+++ src/config.py
@@ -15,3 +15,22 @@ VALID_BOT_PERMISSIONS = {"annihilate","fly","baritone","attack","place","break"}
 BOT_PERMISSION_DEFAULTS = ["baritone","attack"]
 BOT_LOBBY_PERMISSIONS = ["fly","baritone","attack"]
 TIMEOUT = 10
+
+COLOURS = {
+    "black": "#000000",
+    "dark_blue": "#0000AA",
+    "dark_green": "#00AA00",
+    "dark_aqua": "#00AAAA",
+    "dark_red": "#AA0000",
+    "dark_purple": "#AA00AA",
+    "gold": "#FFAA00",
+    "gray": "#AAAAAA",
+    "dark_gray": "#555555",
+    "blue": "#5555FF",
+    "green": "#55FF55",
+    "aqua": "#55FFFF",
+    "red": "#FF5555",
+    "light_purple": "#FF55FF",
+    "yellow": "#FFFF55",
+    "white": "#FFFFFF"
+}
