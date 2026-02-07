999ef28b2f539fa740852c2e3fbf45bf3d991bba
AspectOfTheGit <151046557+AspectOfTheGit@users.noreply.github.com>
2026-02-07 18:20:27 +0000

    Update voice.py

--- src/api/voice.py
+++ src/api/voice.py
@@ -20,12 +20,11 @@ voice = Blueprint(
 def apivoiceupdate():
     match = re.search(r"world:([a-zA-Z0-9-]+)", request.headers.get("User-Agent", ""))
     match = match.group(1) if match else False
+
+    value = request.get_json(silent=True)
     
-    rdata = request.get_json()
-    account = rdata.get("account", "")
-    world = rdata.get("world", match)
-    value = rdata.get("value", "")
-    token = rdata.get("token", "")
+    token = request.headers.get("token", "")
+    world = request.headers.get("world", match)
 
     if not world:
         return jsonify({"error": "No world provided"}), 400
@@ -33,14 +32,12 @@ def apivoiceupdate():
     if world not in data["world"]:
         return jsonify({"error": "World not registered"}), 400
 
-    if account not in data["account"]:
-        return jsonify({"error": "Account doesn't exist"}), 400
+    try:
+        if token != data["world"][world]["token"]["voice"]:
+            return jsonify({"error": "Unauthorized"}), 401
+    except:
+        return jsonify({"error": "No Token Generated"}), 400
 
-    if account != data["world"][world]["owner"]:
-        return jsonify({"error": "Unauthorized"}), 401
-
-    #if token != data["account"][account]["token"]["deploy"]:# todo - create a token for this
-    #    return jsonify({"error": "Unauthorized"}), 401
 
     data["world"][world]["voice"] = time.time_ns() // 1000000
     emit_log('update',"HI, i'm an update",f"voice-{world}")
