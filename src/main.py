413a9e19cf268b7133e47c62d449cca827310f3b
AspectOfTheGit <151046557+AspectOfTheGit@users.noreply.github.com>
2026-02-10 11:55:39 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -57,5 +57,5 @@ def create_app():
 
 app = create_app()
 
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=10000)
+#if __name__ == "__main__":
+#    socketio.run(app, host="0.0.0.0", port=10000)
