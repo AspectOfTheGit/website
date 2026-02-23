422d63da30063b5523cf34dbe3eebb8362edd7d2
AspectOfTheGit <151046557+AspectOfTheGit@users.noreply.github.com>
2026-02-23 12:34:47 +0000

    Re-add all bots
    Bot availability means there will be less confusion so all bots can be added back

--- src/config.py
+++ src/config.py
@@ -10,8 +10,7 @@ GUILD_ID = 1460692900533899438
 REDIRECT_URI = "https://aspectofthe.site/login"
 DATA_FILE = "/data/values.json"
 
-ALL_BOTS = ["AspectOfTheBot","AspectOfTheButt","AspectOfThePoop","AspectOfTheNuts","AspectOfTheCream","AspectOfTheSacks"] # Registered bots
-BOTS = ["AspectOfTheBot","AspectOfTheButt","AspectOfThePoop"] # Processed bots
+BOTS = ["AspectOfTheBot","AspectOfTheButt","AspectOfThePoop","AspectOfTheNuts","AspectOfTheCream","AspectOfTheSacks"]
 VALID_BOT_PERMISSIONS = {"annihilate","fly","baritone","attack","place","break"}
 BOT_PERMISSION_DEFAULTS = ["baritone","attack"]
 BOT_LOBBY_PERMISSIONS = ["fly","baritone","attack"]
