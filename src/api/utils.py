58fce33ea853342295786fb7d4c3c54be485c352
AspectOfTheGit <151046557+AspectOfTheGit@users.noreply.github.com>
2026-02-27 14:27:27 +0000

    Update utils.py

--- src/api/utils.py
+++ src/api/utils.py
@@ -23,3 +23,21 @@ def getworlduuid():
         return jsonify({"error": "Not sent from a world"}), 400
 
     return jsonify({"world": match}), 200
+
+
+@utils.post("/profile/<username>")
+def profilewithusername(username):
+    if not username:
+        return jsonify({"error": "Please provide a username"}), 400
+
+    url = f"https://api.mojang.com/users/profiles/minecraft/{username}"
+    try:
+        r = requests.get(url, timeout=5)
+        if r.status_code == 200:
+            data = r.json()
+        else:
+            return jsonify({"error": "Server did not return status 200"}), 500
+    except Exception:
+        return return jsonify({"error": "Exception on sending request"}), 500
+
+    return jsonify({"profile": data}), 200
