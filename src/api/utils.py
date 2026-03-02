3321d47b196f505f05b367633f1c099365e55a79
AspectOfTheGit <151046557+AspectOfTheGit@users.noreply.github.com>
2026-03-02 09:53:43 +0000

    Update utils.py

--- src/api/utils.py
+++ src/api/utils.py
@@ -66,3 +66,63 @@ def profilewithusernamewithkey(username, key):
             return jsonify({"error": "Invalid key"}), 400
 
     return jsonify(data), 200
+
+
+@utils.post("/format-uuid")
+def requestformatuuid():# todo Add the array format: convert to AND convert from
+    rdata = request.get_json()
+    uuid = rdata.get("uuid", "")
+    format = rdata.get("format", "")
+    
+    if not uuid:
+        return jsonify({"error": "Please provide a UUID"}), 400
+
+    if not format:
+        return jsonify({"error": "Please provide a format type"}), 400
+
+    if format == "hyphenated":
+        u = uuid.replace("-", "").strip()
+        uuid = (
+            u[:8] + "-" +
+            u[8:12] + "-" +
+            u[12:16] + "-" +
+            u[16:20] + "-" +
+            u[20:]
+        )
+
+    if format == "unhyphenated":
+        uuid = uuid.replace("-", "").strip()
+
+    if format == "array":
+        return jsonify({"error": "Array format wip"}), 404
+
+    return jsonify(uuid), 200
+    
+
+@utils.post("/legitidev/player/<uuid>/rank")
+def legitidevplayerrank(uuid):
+    rdata = request.get_json()
+    format = rdata.get("format", "default")
+    
+    if not uuid:
+        return jsonify({"error": "Please provide a UUID"}), 400
+
+    url = f"https://api.legiti.dev/player/{uuid}"
+    try:
+        r = requests.get(url, timeout=5)
+        if r.status_code == 200:
+            data = r.json()
+        else:
+            return jsonify({"error": "Server did not return status 200"}), 500
+    except Exception:
+        return jsonify({"error": "Exception on sending request"}), 500
+
+    data = data.get("rank","Unknown Player")
+    if data == "Unknown Player":
+        return jsonify({"error": "Player not registered on LegitiDevs"}), 404
+        
+
+    if format == "prefix":
+        data = "wip"
+
+    return jsonify(data), 200
