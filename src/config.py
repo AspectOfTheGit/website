bc7042188f0a9d55cd0f888d182582848d0efcc5
AspectOfTheGit <aspect.mc.housing@gmail.com>
2026-07-29 22:22:29 +0100

    bot annihilate in lobby >:) temporary

--- src/config.py
+++ src/config.py
@@ -23,7 +23,7 @@ DATA_FILE = "/data/values.json"
 BOTS = ["AspectOfTheBot","AspectOfTheButt","AspectOfThePoop","AspectOfTheNuts","AspectOfTheCream","AspectOfTheSacks"] # Valid bots
 VALID_BOT_PERMISSIONS = {"annihilate","fly","baritone","attack","place","break"} # Permissions that can be given to the bot
 BOT_PERMISSION_DEFAULTS = ["baritone","attack"] # Permissions given to the bot by default while in any world
-BOT_LOBBY_PERMISSIONS = ["fly","baritone","attack"] # Permissions given to the bot while in lobby
+BOT_LOBBY_PERMISSIONS = ["fly","baritone","attack","annihilate"] # Permissions given to the bot while in lobby
 TIMEOUT = 10 # in seconds, how long the backend will wait until the bot is marked as offline
 AUTO_SAVE_INTERVAL_MINUTES = 15 # In minutes, how often dirty data should be flushed automatically. Set to 0 to disable periodic auto-save
 
